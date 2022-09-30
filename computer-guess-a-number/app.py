6f639e9b7ff00024994062f478fb2e6976f0303d
Sadique <mhsadique1307@gmail.com>
2022-09-30 20:25:24 +0600

    Kylie Ying's version of computer-guess-a-number done

--- computer-guess-a-number/app.py
+++ computer-guess-a-number/app.py
@@ -7,21 +7,40 @@
 
 from random import randint
 
-number_to_guess = int(input('Guess a number between 1 and 100: '))
+# MY IMPLEMENTATION 
+# number_to_guess = int(input('Guess a number between 1 and 100: '))
+# def computer_guess(number_to_guess):
+#     min = 1
+#     max = 1000
+#     count = 0
+#     guessed_by_computer = randint(min, max)
 
-def computer_guess(number_to_guess):
-    min = 1
-    max = 1000
-    count = 0
-    guessed_by_computer = randint(min, max)
+#     while number_to_guess != guessed_by_computer:
+#         guessed_by_computer = randint(min, max)
+#         count = count + 1
+#         if guessed_by_computer > number_to_guess:
+#             max = guessed_by_computer - 1
+#         elif guessed_by_computer < number_to_guess:
+#             min = guessed_by_computer + 1
+#     return f"Made it after {count} guesses!"
 
-    while number_to_guess != guessed_by_computer:
-        guessed_by_computer = randint(min, max)
-        count = count + 1
-        if guessed_by_computer > number_to_guess:
-            max = guessed_by_computer - 1
-        elif guessed_by_computer < number_to_guess:
-            min = guessed_by_computer + 1
-    return f"Made it after {count} guesses!"
+# print(computer_guess(number_to_guess))
 
-print(computer_guess(number_to_guess))
+# Kylie Ying's IMPLEMENTATION 
+def computer_guess2(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = randint(low, high)
+        else:
+            guess = low # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?');
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Yay! The computer guessed the number, {guess} correctly!')
+
+computer_guess2(10)
